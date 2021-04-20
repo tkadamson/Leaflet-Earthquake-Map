f01d18e0902c4a740e2d29ad45b431fb3c5f9a46
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-04-20 12:47:23 -0700

    Erased given sgtarter code (will add back later) and set up an blank map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,15 @@
-/* NOTE FOR STEP 2
-/  You can use the javascript Promise.all function to make two d3.json calls, 
-/  and your then function will not run until all data has been retreived.
-/
-/ ----------------------------------------
-/  Promise.all(
-/    [
-/        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
-/        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
-/    ]
-/  ).then( ([data,platedata]) => {
-/
-/        console.log("earthquakes", data)
-/        console.log("tectonic plates", platedata)
-/
-/    }).catch(e => console.warn(e));
-/
-/ ----------------------------------------*/
+// Create a map object
+const myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 3
+  });
+  
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
